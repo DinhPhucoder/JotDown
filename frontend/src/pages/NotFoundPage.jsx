2640230641058c58f9f12f1e7a96343e85ee54b0
Phan Đình Phú <52300051@student.tdtu.edu.vn>
2026-05-04 04:41:45 +0700

    clean codebase

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -4,7 +4,7 @@ import './NotFoundPage.css';
 
 const NotFound = () => {
     return (
-        <div className="not-found-page" style={{ backgroundImage: `url(/404.png)` }}>
+        <div className="not-found-page" style={{ backgroundImage: `url(https://res.cloudinary.com/dkrag40hw/image/upload/v1777844340/note-attachments/yo2xdfbckgimlm8tygux.png)` }}>
             <div className="not-found-overlay" />
             <div className="not-found-content">
                 <h1 className="not-found-title" data-text="404">
